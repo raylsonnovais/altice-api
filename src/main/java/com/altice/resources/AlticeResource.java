779e538f3c0e04248400740514aa17f4fcb65fca
raylson11 <raylson11@gmail.com>
2022-06-26 01:19:40 -0300

    Ajuste do resouce

--- src/main/java/com/altice/resources/AlticeResource.java
+++ src/main/java/com/altice/resources/AlticeResource.java
@@ -4,6 +4,8 @@ import com.altice.services.AlticeService;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -23,6 +25,12 @@ public class AlticeResource {
     @Path("/{n}")
     public Response sequenciaAlticci(@PathParam("n") Integer n){
         return Response.ok(service.sequenciaAltice(n)).build();
+    }
+    
+    @GET
+    @Path("all/{n}")
+    public List<Response> getAllsequenciaAlticci(@PathParam("n") Integer n){
+        return (List<Response>) Response.ok(service.allSequenceAltice(n)).build();
     }
     
     
--- src/main/java/com/altice/services/AlticeService.java
+++ src/main/java/com/altice/services/AlticeService.java
@@ -15,14 +15,23 @@ public class AlticeService {
     public Integer sequenciaAltice(Integer n){
         return n > 2 ? maiorQueDois(n) : menorQueDois(n);
     }
-
-    private Integer maiorQueDois(Integer n){
-        for(int count = 0; count < n ;count++){
+    
+    public List<Integer> allSequenceAltice(Integer n){
+    	return mountList(n);
+    }
+    
+    private List<Integer> mountList(Integer n){
+    	for(int count = 0; count < n ;count++){
         	if(count + 1 >= altice.size()) {
         		altice.add(altice.get(n - 3) + altice.get(n - 2));
         	}	
         }
-        return altice.get(n);
+    	return altice;
+    }
+
+    private Integer maiorQueDois(Integer n){
+    	List<Integer> list = mountList(n);
+        return list.get(n);
     }
 
     private Integer menorQueDois(Integer n){
